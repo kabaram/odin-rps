9322c52e03974cddaae41208d872175a98a81b2f
Kim Baram <katbaram@gmail.com>
2023-08-30 14:43:28 -0400

    Add win event, sound, and visual effects

--- rps.js
+++ rps.js
@@ -1,3 +1,4 @@
+//maintains location and size data of subimages from sprite sheet
 class Sprite {
     constructor(x, y, width, height, docClass) {
         this.x = x;
@@ -35,6 +36,7 @@ class Sprite {
 
 }
 
+//maintains action and image data on each ability
 class Ability {
     constructor(name, verb, choiceSprite, winSprite, loseSprite, arenaValue) {
         this.name = name;
@@ -46,43 +48,8 @@ class Ability {
     }
 }
 
-//hex background color of sprite sheet
-const SPRITE_SHEET_BACKGROUND = "749AD4";
 
-const IMAGE_SOURCE = "./images/kirby-ss.png";
-
-const SWORD_CHOICE_SPRITE = new Sprite(308, 1030, 18, 31, '.sword-choice-canvas');
-SWORD_CHOICE_SPRITE.showAbility();
-const SWORD_WIN_SPRITE = new Sprite(266, 1026, 20, 35);
-const SWORD_LOSE_SPRITE = new Sprite(17, 1148, 16, 23);
-const SWORD_ABILITY = new Ability('Sword', 'slash', SWORD_CHOICE_SPRITE, SWORD_WIN_SPRITE, SWORD_LOSE_SPRITE, 2);
-
-const STONE_CHOICE_SPRITE = new Sprite(162, 796, 22, 16, '.stone-choice-canvas');
-STONE_CHOICE_SPRITE.showAbility();
-const STONE_WIN_SPRITE = new Sprite(134, 796, 22, 19);
-const STONE_LOSE_SPRITE = new Sprite(85, 797, 16, 14);
-const STONE_ABILITY = new Ability('Stone', 'crush', STONE_CHOICE_SPRITE, STONE_WIN_SPRITE, STONE_LOSE_SPRITE, 0);
-
-const PARASOL_CHOICE_SPRITE = new Sprite(168, 1425, 20, 31, '.parasol-choice-canvas');
-PARASOL_CHOICE_SPRITE.showAbility();
-const PARASOL_WIN_SPRITE = new Sprite(197, 1442, 20, 30);
-const PARASOL_LOSE_SPRITE = new Sprite(16, 1557, 17, 26);
-const PARASOL_ABILITY = new Ability('Parasol', 'cover', PARASOL_CHOICE_SPRITE, PARASOL_WIN_SPRITE, PARASOL_LOSE_SPRITE, 1);
-
-const stoneButton = document.querySelector('.stone');
-const parasolButton = document.querySelector('.parasol');
-const swordButton = document.querySelector('.sword');
-
-stoneButton.addEventListener('click', function () { playOneRound(0) });
-parasolButton.addEventListener('click', function () { playOneRound(1) });
-swordButton.addEventListener('click', function () { playOneRound(2) });
-
-
-let playerScore = 0;
-let computerScore = 0;
-
-
-
+//Produces ability based on random number generation
 function getComputerAbility() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
@@ -96,6 +63,7 @@ function getComputerAbility() {
     }
 }
 
+//Maps value associated with selected button to the ability
 function getHumanAbility(choice) {
     if (choice === 0) {
         return STONE_ABILITY;
@@ -108,19 +76,37 @@ function getHumanAbility(choice) {
     }
 }
 
+//Uses player's button selection to simulate round
 function playOneRound(humanChoice, computerAbility = getComputerAbility()) {
 
+    let humanAbility = getHumanAbility(humanChoice);
+
+    //Re-adjust layout of the result container to accommodate icons
     let resultIcons = document.querySelectorAll('.result-icon-cell');
     resultIcons.forEach((icon) => {
         icon.style.display = "flex";
     })
-    let humanAbility = getHumanAbility(humanChoice);
     let textField = document.querySelector('.result-text-cell');
     textField.style.width = "30%";
+
+
+
+    //Update score panels
+    let playerScoreField = document.querySelector('.player-score');
+    playerScoreField.innerHTML = "Player: " + playerScore;
+    let computerScoreField = document.querySelector('.computer-score');
+    computerScoreField.innerHTML = "Computer: " + computerScore;
+    let winner = document.querySelector('.winner-display');
+    winner.innerHTML = '';
+
+    let audio;
+
+    //Calculate round winner
     if (humanAbility.arenaValue === computerAbility.arenaValue) {
         humanAbility.winSprite.showPlayerResult();
         computerAbility.winSprite.showComputerResult();
         textField.innerHTML = "Draw! Both players chose " + humanAbility.name + ".";
+        audio = document.querySelector('#draw-sound');
     }
 
     else if ((humanAbility.arenaValue + 1) % 3 == computerAbility.arenaValue) {
@@ -128,19 +114,90 @@ function playOneRound(humanChoice, computerAbility = getComputerAbility()) {
         computerAbility.winSprite.showComputerResult();
         textField.innerHTML = "Fail! Your " + humanAbility.name + " is " + computerAbility.verb + "ed by the computer's " + computerAbility.name + ".";
         computerScore++;
-        let scoreField = document.querySelector('.computer-score');
-        scoreField.innerHTML = "Computer: " + computerScore;
+        computerScoreField.innerHTML = "Computer: " + computerScore;
+        audio = document.querySelector('#lose-sound');
     }
     else {
         humanAbility.winSprite.showPlayerResult();
         computerAbility.loseSprite.showComputerResult();
         textField.innerHTML = "Victory! Your " + humanAbility.name + " " + humanAbility.verb + (humanAbility.name !== 'Parasol' ? 'e' : '') + "s the computer's " + computerAbility.name + ".";
         playerScore++;
-        let scoreField = document.querySelector('.player-score');
-        scoreField.innerHTML = "Player: " + playerScore;
+        playerScoreField.innerHTML = "Player: " + playerScore;
+        audio = document.querySelector('#win-sound');
+    }
+
+    audio.play();
+
+    // Declare game victor and reset scores
+    if (playerScore == 5) {
+        winner.innerHTML = "You win!";
+        winner.style.color = "lightpink";
+        playerScore = computerScore = 0;
+    }
+    else if (computerScore == 5) {
+        winner.innerHTML = "Computer wins!";
+        winner.style.color = 'crimson';
+        playerScore = computerScore = 0;
     }
 }
 
+//Game initializations
+
+let playerScore = 0;
+let computerScore = 0;
+
+//Hex background color of sprite sheet
+const SPRITE_SHEET_BACKGROUND = "749AD4";
+
+//Sprite sheet
+const IMAGE_SOURCE = "./images/kirby-ss.png";
+
+const STONE_CHOICE_SPRITE = new Sprite(162, 796, 22, 16, '.stone-choice-canvas');
+const STONE_WIN_SPRITE = new Sprite(134, 796, 22, 19);
+const STONE_LOSE_SPRITE = new Sprite(85, 797, 16, 14);
+const STONE_ABILITY = new Ability('Stone', 'crush', STONE_CHOICE_SPRITE, STONE_WIN_SPRITE, STONE_LOSE_SPRITE, 0);
+
+const PARASOL_CHOICE_SPRITE = new Sprite(168, 1425, 20, 31, '.parasol-choice-canvas');
+const PARASOL_WIN_SPRITE = new Sprite(197, 1442, 20, 30);
+const PARASOL_LOSE_SPRITE = new Sprite(16, 1557, 17, 26);
+const PARASOL_ABILITY = new Ability('Parasol', 'cover', PARASOL_CHOICE_SPRITE, PARASOL_WIN_SPRITE, PARASOL_LOSE_SPRITE, 1);
+
+const SWORD_CHOICE_SPRITE = new Sprite(308, 1030, 18, 31, '.sword-choice-canvas');
+const SWORD_WIN_SPRITE = new Sprite(266, 1026, 20, 35);
+const SWORD_LOSE_SPRITE = new Sprite(17, 1148, 16, 23);
+const SWORD_ABILITY = new Ability('Sword', 'slash', SWORD_CHOICE_SPRITE, SWORD_WIN_SPRITE, SWORD_LOSE_SPRITE, 2);
+
+
+//Add gameplay function to buttons
+const stoneButton = document.querySelector('.stone-choice');
+const parasolButton = document.querySelector('.parasol-choice');
+const swordButton = document.querySelector('.sword-choice');
+
+stoneButton.addEventListener('click', function () { playOneRound(0) });
+parasolButton.addEventListener('click', function () { playOneRound(1) });
+swordButton.addEventListener('click', function () { playOneRound(2) });
+
+
+stoneButton.addEventListener('mouseenter', function () { stoneButton.classList.add('hovering'); });
+stoneButton.addEventListener('mouseleave', function(){stoneButton.classList.remove('hovering', 'clicking');});
+stoneButton.addEventListener('mousedown', function () { stoneButton.classList.add('clicking'); });
+stoneButton.addEventListener('mouseup', function(){stoneButton.classList.remove('clicking');});
+parasolButton.addEventListener('mouseenter', function () { parasolButton.classList.add('hovering'); });
+parasolButton.addEventListener('mouseleave', function(){parasolButton.classList.remove('hovering', 'clicking');});
+parasolButton.addEventListener('mousedown', function () {parasolButton.classList.add('clicking'); });
+parasolButton.addEventListener('mouseup', function(){parasolButton.classList.remove('clicking');});
+swordButton.addEventListener('mouseenter', function () { swordButton.classList.add('hovering'); });
+swordButton.addEventListener('mouseleave', function() {swordButton.classList.remove('hovering', 'clicking');});
+swordButton.addEventListener('mousedown', function () { swordButton.classList.add('clicking'); });
+swordButton.addEventListener('mouseup', function(){swordButton.classList.remove('clicking');});
+
+
+//Display ability buttons
+PARASOL_CHOICE_SPRITE.showAbility();
+STONE_CHOICE_SPRITE.showAbility();
+SWORD_CHOICE_SPRITE.showAbility();
+
+//Hide player icons at start of game
 let resultIcons = document.querySelectorAll('.result-icon-cell');
 resultIcons.forEach((icon) => {
     icon.style.display = "none";
@@ -148,30 +205,5 @@ resultIcons.forEach((icon) => {
 let resultText = document.querySelector('.result-text-cell');
 resultText.style.width = "100%";
 
-// function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (let i = 1; i <= 5; i++) {
-//         let playerChoice = prompt("Round #" + i + ": Choose: (0 - Stone) (1 - Parasol) (3 - Sword)");
-//         let result = playOneRound(playerChoice);
-//         if (result.includes("Fail")) {
-//             computerScore++;
-//         }
-//         else if (result.includes("Victory")) {
-//             playerScore++;
-//         }
-//         console.log(result);
-//         console.log("Player: " + playerScore + ", Computer: " + computerScore);
-//     }
-//     if (playerScore > computerScore) {
-//         console.log("You win!");
-//     }
-//     else if (computerScore > playerScore) {
-//         console.log("Computer wins!");
-//     }
-//     else {
-//         console.log("It's a tie!");
-//     }
-//}
 
 
